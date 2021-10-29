6d3e4be3e819f555fcef8e2cd3d4d2263c42fa78
Filip Mularski <mularskif@gmail.com>
2021-10-29 21:37:35 +0200

    styled register page

--- shapes/views.py
+++ shapes/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .forms import RegisterForm
 
 
 class RegisterPageView(TemplateView):
     template_name = 'shapes/register.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = RegisterForm
+        return context
