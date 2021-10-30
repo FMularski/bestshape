275c74dc22ec3e971e07cf8568aaa9e522d95bf7
Filip Mularski <mularskif@gmail.com>
2021-10-31 00:39:23 +0200

    created ShapeSerializer class

--- shapes/api/serializers.py
+++ shapes/api/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
-from shapes.models import User
+from shapes.models import User, Shape
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+    
+class ShapeSerializer(ModelSerializer):
+    class Meta:
+        model = Shape
         fields = '__all__'
         
