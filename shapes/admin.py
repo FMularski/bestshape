ad56b01c344272ca78bbb128fbd3ebb763aa9cec
Filip Mularski <mularskif@gmail.com>
2021-10-31 00:34:56 +0200

    created shape model

--- shapes/admin.py
+++ shapes/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Shape
+
+
+class ShapeAdmin(admin.ModelAdmin):
+    list_display = 'name', 'image' 
 
 
 admin.site.register(User, UserAdmin)
-
+admin.site.register(Shape, ShapeAdmin)
 
--- shapes/models.py
+++ shapes/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    pass
+from django.dispatch import receiver
+from django.db.models.signals import post_delete
 
 
 class Shape(models.Model):
+    name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='shapes')
+
+
+class User(AbstractUser):
+    shape = models.ForeignKey(Shape, on_delete=models.SET_NULL, null=True)
+
+
+@receiver(post_delete, sender=Shape)
+def delete_shape_handler(instance, *args, **kwargs):
+    instance.image.delete()
+
